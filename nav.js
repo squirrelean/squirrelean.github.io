8a60e9ae69e004e223502f0a129e30ee0375de0e
Daniel <danastasio02@icloud.com>
2025-10-20 10:53:25 -0400

    feat: add navigation logic

---
+++ nav.js
@@ -0,0 +1,31 @@
+function splitAtRoot(path)
+{
+    const url = new URL(path, location.origin);
+    const pathFromRoot = url.pathname;
+    document.write("<br>----> path from root: " + pathFromRoot);
+    return pathFromRoot;
+}
+
+function setNav(current_path)
+{
+    current_path = splitAtRoot(current_path);
+    let nav = document.getElementById("main-nav");
+    for (let child of nav.children)
+    {
+        if (child instanceof HTMLAnchorElement)
+        {
+            let page = child.href;
+            page = splitAtRoot(page);
+            if (page === current_path)
+            {
+                child.classList.add("current_page");
+            }
+        }
+    }
+    
+}
+
+fetch("nav.html")
+.then(r => r.text())
+.then(html => document.getElementById("main-nav").innerHTML = html);
+
