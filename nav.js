849214711eab4b2dcf63d8993629cdfcf9b26608
Daniel <danastasio02@icloud.com>
2025-10-26 09:05:25 -0400

    feat: add navigation script to multiple HTML files

--- nav.js
+++ nav.js
@@ -2,7 +2,6 @@ function splitAtRoot(path)
 {
     const url = new URL(path, location.origin);
     const pathFromRoot = url.pathname;
-    document.write("<br>----> path from root: " + pathFromRoot);
     return pathFromRoot;
 }
 
@@ -10,16 +9,12 @@ function setNav(current_path)
 {
     current_path = splitAtRoot(current_path);
     let nav = document.getElementById("main-nav");
-    document.write("function exec");
     for (let child of nav.children)
     {
-        document.write("loop exec");
         if (child instanceof HTMLAnchorElement)
         {
             let page = child.href;
             page = splitAtRoot(page);
-            document.write("<p>current path: " + current_path);
-            document.write("page: " + page + "</p>");
             if (page === current_path)
             {
                 child.classList.add("current_page");
@@ -31,5 +26,9 @@ function setNav(current_path)
 
 fetch("nav.html")
 .then(r => r.text())
-.then(html => document.getElementById("main-nav").innerHTML = html);
+.then(html => {
+    (document.getElementById("main-nav").innerHTML = html);
+})
+
+//.then(html => document.getElementById("main-nav").innerHTML = html);
 
